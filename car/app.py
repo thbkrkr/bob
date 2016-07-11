8be87f5107f60ec9336c47c64dd8b90297cf1bac
thb <thb.krkr@gmail.com>
2016-07-11 21:18:06 +0200

    bam!

---
+++ car/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+  return app.send_static_file('index.html')
+
+@app.route("/api/forward")
+def forward():
+  return "Forward"
+
+@app.route("/api/back")
+def back():
+  return "back"
+
+@app.route("/api/left")
+def left():
+  return "Left"
+
+@app.route("/api/right")
+def right():
+  return "Right"
+
+if __name__ == "__main__":
+  app.run()
